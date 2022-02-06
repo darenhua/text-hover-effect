4b19f036b5889e926c2876a3a97e580f3f92d879
Daren Hua <dhua2021@erhsnyc.net>
2022-02-05 22:54:32 -0500

    text hover upload project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+
+const App = () => {
+  const [active, setActive] = useState(false);
+  const clickHandler = () => setActive(!active);
+  console.log("hi!");
+  return (
+    <span className={`${active && "active"}`} onClick={clickHandler}>
+      Hello
+    </span>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
